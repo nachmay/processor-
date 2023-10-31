fc31d8df4373f184a9befc8f2d7d204463748568
nachman <abergel.nachman@mail.huji.ac.il>
2023-10-31 14:15:38 +0200

    all

--- Day1/day1_tb.sv
+++ Day1/day1_tb.sv
@@ -4,16 +4,16 @@ module mux2to1_tb;
 	reg sel ;
 	wire [7:0]out;
 
-	mux2to1(.a(a), .b(b), .sel(sel), .out(out))
+	mux2to1 show(.a(a), .b(b), .sel(sel), .out(out));
 
 	initial
 		$monitor("time=%d, a=%b, b=%b, sel=%b, out=%b", $time, a, b, sel, out);
 	initial begin 
 		for (int i=0; i<=10; i++)begin
-			$urandom_range(0, 8'hFF);
-			$urandom_range(0, 8'hFF);
+			a = $urandom_range(0, 8'hFF);
+			b = $urandom_range(0, 8'hFF);
 			sel = $random%2;
-			#5
+			#5;
 		end
 	end
 endmodule	
