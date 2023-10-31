4ef4652c86997218eb319ef015448311052707e0
nachman <abergel.nachman@mail.huji.ac.il>
2023-10-31 14:04:56 +0200

    correct code

--- Day1/day_one.sv
+++ Day1/day_one.sv
@@ -1,7 +1,8 @@
 
-module nux2to1(out, A, B, sel);
-	input A, B [7:0];
+module n
+mux2to1(out, a, b, sel);
+	input [7:0] a, b;
 	input sel;
-	output out;
-	assign out = sel ? A : B;
+	output [7:0] out;
+	assign out = sel ? a :b;
 endmodule
