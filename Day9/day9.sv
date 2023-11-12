e75021ffae458794e54c1989be1a2a4176851990
nachman <abergel.nachman@mail.huji.ac.il>
2023-11-12 14:01:06 +0200

    day9 done and pass testbench

--- Day9/day9.sv
+++ Day9/day9.sv
@@ -3,7 +3,7 @@ module bin_to_gray(gray_out, bin_in);
 	input wire [3:0]bin_in;
 	output wire [3:0] gray_out;
 
-	assign gray_out[3] = bin_in[3]
+	assign gray_out[3] = bin_in[3l];
 	genvar i;
 
 	for ( int i =2; i>=0; i = i-1)
